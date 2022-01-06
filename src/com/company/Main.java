db0f49c55e04a6ce4aa39c046422964c07503614
Sakshi Shetty <sakshishetty@Sakshis-MacBook-Pro.local>
2022-01-06 14:30:45 +0530

    Add. Implementation of class and object

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Main method is static because the compile can call the method without creation of the object");
+    }
+}
