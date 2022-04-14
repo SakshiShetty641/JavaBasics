f2a720b61e11794d8eae79b6e4cd9a8bd95e8868
Sakshi Shetty <sakshishetty@Sakshis-MacBook-Pro.local>
2022-04-14 12:36:19 +0530

    To demonstrate the concept of Math class and its function

---
+++ src/com/company/javabasics/MathClass.java
@@ -0,0 +1,14 @@
+package com.company.javabasics;
+
+public class MathClass {
+    public static void main(String[] args) {
+        //max
+        System.out.println(Math.max(7, 8));
+
+        //min
+        System.out.println(Math.min(7, 5));
+
+        //random
+        System.out.println((int)(Math.random() * 100));
+    }
+}
