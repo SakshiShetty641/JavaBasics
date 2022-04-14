c43135948d0102d0d0f1c77e48a6d98fc7b02bcd
Sakshi Shetty <sakshishetty@Sakshis-MacBook-Pro.local>
2022-04-14 12:18:24 +0530

    To demonstrate the use of final keyword

---
+++ src/com/company/javabasics/Constants.java
@@ -0,0 +1,18 @@
+package com.company.javabasics;
+
+public class Constants {
+    public static void main(String[] args) {
+        // TODO: 14/04/22 -> final keyword - once declared as final cannot change the value and should be defined in upper case
+
+        // ==> value keeps on changing for int
+        int age = 30;
+        age = 32;
+
+        /**
+         * The final keyword is a non-access modifier used for classes, attributes and methods,
+         * which makes them non-changeable (impossible to inherit or override).
+         */
+       final float PI = 3.14F;
+
+    }
+}
