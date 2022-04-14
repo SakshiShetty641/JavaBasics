acb65e4a59b33c88a62a4fb724463d4a1940e785
Sakshi Shetty <sakshishetty@Sakshis-MacBook-Pro.local>
2022-04-14 12:52:17 +0530

    To demonstrate the concept of Scanner class

---
+++ src/com/company/javabasics/Inputs.java
@@ -0,0 +1,26 @@
+package com.company.javabasics;
+
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args) {
+        //To take input System.in
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter your age: ");
+        int age = sc.nextInt();
+        System.out.println(age);
+
+        // For Strings
+        System.out.println(" Please enter your name :");
+        String name = sc.next();  // ==> next will only print one word even if you pass an entire statement
+        System.out.println(name);
+
+
+        // To print entire line ==> use sc.nextline
+        System.out.println("Please enter address :");
+        String line =  sc.nextLine();
+        System.out.println(line);
+
+    }
+}
