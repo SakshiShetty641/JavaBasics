0f658413e71ccfa6a28c7d0be62ca4f6cacd1b37
Sakshi Shetty <sakshishetty@Sakshis-MacBook-Pro.local>
2022-04-18 15:20:39 +0530

    Use of this keyword in java

--- src/com/company/OOPS/ClassObject.java
+++ src/com/company/OOPS/ClassObject.java
@@ -3,10 +3,11 @@ package com.company.OOPS;
 // TODO: 18/04/22 - Defination of class and object
 
 /**
- * Class - A class is a logical entity, A class does not allocate memory space when it is created, You can declare class only once.
- * Object - Object is a physical entity, Object allocates memory space whenever they are created, You can create more than one object using a class.
- * new keyword - The Java new keyword is used to create an instance of the class. In other words,
- * it instantiates a class by allocating memory for a new object and returning a reference to that memory.
+ * Class ==> A class is a logical entity, A class does not allocate memory space when it is created, You can declare class only once.
+ * Object ==>  Object is a physical entity, Object allocates memory space whenever they are created, You can create more than one object using a class.
+ * new keyword ==> The Java new keyword is used to create an instance of the class. In other words,
+ *                  it instantiates a class by allocating memory for a new object and returning a reference to that memory.
+ * this keyword ==> The this keyword refers to the current object in a method or constructor.
  */
 
 
@@ -17,6 +18,16 @@ class Pen {
     public void use() {
         System.out.println("The color of the pen is : " + color + " & the type of the pen is : " + type);
         System.out.println("Writing something.....");
+    }
+}
+
+class Student {
+    String name;
+    int age;
+
+    public void display() {
+        System.out.println(this.name);
+        System.out.println(this.age);
     }
 }
 
@@ -31,7 +42,10 @@ public class ClassObject {
         pen2.color = "black";
         pen2.type = "ball pen";
         pen2.use();
+
+        Student student = new Student();
+        student.name = "Sakshi";
+        student.age = 10;
+        student.display();
     }
-
-
 }
---
+++ src/com/company/OOPS/Constructors.java
@@ -0,0 +1,2 @@
+package com.company.OOPS;public class Constructors {
+}
