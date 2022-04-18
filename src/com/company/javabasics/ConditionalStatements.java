d765e82590d67f18632b9a98cb386bb67519e240
Sakshi Shetty <sakshishetty@Sakshis-MacBook-Pro.local>
2022-04-18 11:17:26 +0530

    To demonstrate the use of conditional statments in java

--- src/com/company/javabasics/ConditionalStatements.java
+++ src/com/company/javabasics/ConditionalStatements.java
@@ -14,5 +14,17 @@ public class ConditionalStatements {
             System.out.println("Admission Provided");
         else
             System.out.println("Admission Denied");
+
+        //==> when more than 1 statement inside if then we need to use {}
+        System.out.println("Enter the amount you have : ");
+        int cash = sc.nextInt();
+        if (cash < 50) {
+            System.out.println("You cannot buy anything");
+            System.out.println("Please get more cash");
+        } else if (cash < 100 && cash > 50  )
+            System.out.println("you can buy 1 thing");
+        else
+            System.out.println("You can buy");
     }
+
 }
